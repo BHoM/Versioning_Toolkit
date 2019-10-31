20432f33ccadec359bb5049316842b0dee374a13
adecler <arnaud.declercq@burohappold.com>
2019-10-31 17:09:34 +0800

    Provide an update strategy for change of namespace and/or class name

--- BHoMUpgrader/Upgrader.cs
+++ BHoMUpgrader/Upgrader.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.IO.Pipes;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -27,6 +28,25 @@ namespace BH.Upgrader.Base
                 Console.WriteLine("Assembly Loaded: " + assembly.Name);
             }
             Console.WriteLine("");
+
+            // Gather all the conversion methods from the converter
+            MethodInfo[] converterMethods = converter.GetType().GetMethods();
+            foreach (MethodInfo method in converterMethods.Where(x => x.Name == "ToNew"))
+            {
+                ParameterInfo parameter = method.GetParameters().FirstOrDefault();
+                if (parameter != null)
+                {
+                    switch (method.Name)
+                    {
+                        case "ToNew":
+                            m_ToNewConverters.Add(parameter.ParameterType.FullName);
+                            break;
+                        case "ToOld":
+                            m_ToOldConverters.Add(parameter.ParameterType.FullName);
+                            break;
+                    }
+                }       
+            }
 
             // Deactivate the upgrade check in teh serialiser
             BH.Engine.Serialiser.Compute.AllowUpgradeFromBson(false);
@@ -70,30 +90,86 @@ namespace BH.Upgrader.Base
 
             Console.WriteLine("document received: " + document.ToJson());
 
-            object item = BH.Engine.Serialiser.Convert.FromBson(document);
-            if (item == null)
-                return null;
-
-            Console.WriteLine("object recovered: " + item.GetType().FullName);
-
-            object b = converter.IToNew(item as dynamic);
-            if (b == null)
-                return null;
-
-            Console.WriteLine("object updated: " + b.GetType().FullName);
-
-            BsonDocument newDoc = BH.Engine.Serialiser.Convert.ToBson(b);
-
-            // Copy over BHoM properties
-            string[] properties = new string[] { "BHoM_Guid", "CustomData", "Name", "Tags", "Fragments" }; 
-            foreach (string p in properties)
+            if (document.Contains("_t"))
             {
-                if (newDoc.Contains(p) && document.Contains(p))
-                    newDoc[p] = document[p];
-            }
-            
-            return newDoc;
+                string oldType = document["_t"].AsString;
+                string newType = ToNewType(oldType, converter);
+                document["_t"] = newType;
+                if (newType != oldType)
+                {
+                    Console.WriteLine("type upgraded from " + oldType + " to " + newType);
+                    return document;
+                }  
+                else if (m_ToNewConverters.Contains(oldType))
+                {
+                    object item = BH.Engine.Serialiser.Convert.FromBson(document);
+                    if (item == null)
+                        return null;
 
+                    Console.WriteLine("object recovered: " + item.GetType().FullName);
+
+                    object b = converter.IToNew(item as dynamic);
+                    if (b == null)
+                        return null;
+
+                    Console.WriteLine("object updated: " + b.GetType().FullName);
+                    BsonDocument newDoc = BH.Engine.Serialiser.Convert.ToBson(b);
+
+                    // Copy over BHoM properties
+                    string[] properties = new string[] { "BHoM_Guid", "CustomData", "Name", "Tags", "Fragments" };
+                    foreach (string p in properties)
+                    {
+                        if (newDoc.Contains(p) && document.Contains(p))
+                            newDoc[p] = document[p];
+                    }
+
+                    return newDoc;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        /***************************************************/
+
+        private string ToNewType(string type, IConverter converter)
+        {
+            return GetTypeFromDic(converter.ToNewType, type);
+        }
+
+        /***************************************************/
+
+        private string ToOldType(string type, IConverter converter)
+        {
+            return GetTypeFromDic(converter.ToOldType, type);
+        }
+
+        /***************************************************/
+
+        private static string GetTypeFromDic(Dictionary<string, string> dic, string type)
+        {
+            if (dic.ContainsKey(type))
+                return dic[type];
+            else
+            {
+                int index = type.LastIndexOf('.');
+                if (index > 0)
+                {
+                    string ns = type.Substring(0, index);
+                    if (dic.ContainsKey(ns))
+                        return dic[ns] + type.Substring(index);
+                    else
+                        return type;
+                }
+                else
+                    return type;
+            }
         }
 
         /***************************************************/
@@ -102,6 +178,9 @@ namespace BH.Upgrader.Base
         {
             try
             {
+                if (document == null)
+                    writer.Write(0);
+
                 MemoryStream memory = new MemoryStream();
                 BsonBinaryWriter memoryWriter = new BsonBinaryWriter(memory);
                 BsonSerializer.Serialize(memoryWriter, typeof(BsonDocument), document);
@@ -124,6 +203,8 @@ namespace BH.Upgrader.Base
             try
             {
                 int contentSize = reader.ReadInt32();
+                if (contentSize == 0)
+                    return null;
 
                 byte[] content = new byte[contentSize];
                 reader.Read(content, 0, contentSize);
@@ -135,6 +216,14 @@ namespace BH.Upgrader.Base
                 return null;
             }
         }
+
+
+        /***************************************************/
+        /**** Private Fields                            ****/
+        /***************************************************/
+
+        private HashSet<string> m_ToNewConverters = new HashSet<string>();
+        private HashSet<string> m_ToOldConverters = new HashSet<string>();
 
         /***************************************************/
     }
