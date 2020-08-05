6adf75888d691a0672d60ed375220b152d7c2d52
Isak Naslund <Isak.Naslund@burohappold.com>
2020-08-05 13:34:06 +0200

    Adding full upgrade converts for FramingElements and ConstantFramingElementProperty

--- BHoMUpgrader33/Converter.cs
+++ BHoMUpgrader33/Converter.cs
@@ -36,7 +36,11 @@ namespace BH.Upgrader.v33
 
         public Converter() : base()
         {
-            PreviousVersion = "3.2"; 
+            PreviousVersion = "3.2";
+
+            ToNewObject.Add("BH.oM.Structure.FramingProperties.ConstantFramingElementProperty", UpgradeConstantFramingElementProperty);
+            ToNewObject.Add("BH.oM.Structure.Elements.FramingElement", UpgradeFramingElement);
+
         }
 
 
@@ -44,7 +48,89 @@ namespace BH.Upgrader.v33
         /**** Private Methods                           ****/
         /***************************************************/
 
+        public static Dictionary<string, object> UpgradeConstantFramingElementProperty(Dictionary<string, object> oldVersion)
+        {
+            if (oldVersion == null)
+                return null;
 
+            Dictionary<string, object> newVersion = new Dictionary<string, object>();
+
+            newVersion["_t"] = "BH.oM.Physical.FramingProperties.ConstantFramingProperty";
+            newVersion["OrientationAngle"] = oldVersion["OrientationAngle"];
+            newVersion["Name"] = oldVersion["Name"];
+            newVersion["CustomData"] = oldVersion["CustomData"];
+            newVersion["BHoM_Guid"] = oldVersion["BHoM_Guid"];
+            newVersion["Tags"] = oldVersion["Tags"];
+            newVersion["Fragments"] = oldVersion["Fragments"];
+
+            object sectionProp;
+            if (oldVersion.TryGetValue("SectionProperty", out sectionProp))
+            {
+                Dictionary<string, object> sectionDict = sectionProp as Dictionary<string, object>;
+                object profile;
+                if (sectionDict != null && sectionDict.TryGetValue("SectionProfile", out profile))
+                {
+                    newVersion["Profile"] = profile;
+                }
+
+                object strMaterial;
+                if (sectionDict != null && sectionDict.TryGetValue("Material", out strMaterial))
+                {
+                    Dictionary<string, object> strMatDict = strMaterial as Dictionary<string, object>;
+
+                    if (strMatDict != null)
+                    {
+                        Dictionary<string, object> material = new Dictionary<string, object>();
+                        material["_t"] = "BH.oM.Physical.Materials.Material";
+                        if (strMatDict.ContainsKey("Name"))
+                            material["Name"] = strMatDict["Name"];
+                        material["Properties"] = new List<object> { strMatDict };
+
+                        newVersion["Material"] = material;
+                    }
+                }
+            }
+
+            return newVersion;
+        }
+
+        /***************************************************/
+
+        public static Dictionary<string, object> UpgradeFramingElement(Dictionary<string, object> oldVersion)
+        {
+            if (oldVersion == null)
+                return null;
+
+            Dictionary<string, object> newVersion = new Dictionary<string, object>();
+
+            string type = "Beam";
+            object typeObject;
+            if (oldVersion.TryGetValue("StructuralUsage", out typeObject))
+                type = typeObject.ToString();
+
+            if(type == "Column")
+                newVersion["_t"] = "BH.oM.Physical.Elements.Column";
+            else if(type == "Brace")
+                newVersion["_t"] = "BH.oM.Physical.Elements.Bracing";
+            else if (type == "Cable")
+                newVersion["_t"] = "BH.oM.Physical.Elements.Cable";
+            else if (type == "Pile")
+                newVersion["_t"] = "BH.oM.Physical.Elements.Pile";
+            else
+                newVersion["_t"] = "BH.oM.Physical.Elements.Beam";
+
+
+
+            newVersion["Name"] = oldVersion["Name"];
+            newVersion["CustomData"] = oldVersion["CustomData"];
+            newVersion["BHoM_Guid"] = oldVersion["BHoM_Guid"];
+            newVersion["Tags"] = oldVersion["Tags"];
+            newVersion["Fragments"] = oldVersion["Fragments"];
+            newVersion["Location"] = oldVersion["LocationCurve"];
+            newVersion["Property"] = oldVersion["Property"];
+
+            return newVersion;
+        }
 
         /***************************************************/
 
