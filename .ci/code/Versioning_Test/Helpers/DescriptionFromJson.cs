8ef41eb930670d7569b940204c8afb203939801b
adecler <arnaud.declercq@burohappold.com>
2020-12-12 19:13:38 +0800

    Create CI tests for versioning

---
+++ .ci/code/Versioning_Test/Helpers/DescriptionFromJson.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BH.Test.Versioning
+{
+    public static partial class Helpers
+    {
+        /*************************************/
+        /**** Public Methods              ****/
+        /*************************************/
+
+        public static string DescriptionFromJson(string json)
+        {
+            string[] split = json.Split(new char[] { '"' });
+            if (split.Length < 4)
+                return "";
+
+            string type = split[3];
+            if (type == "System.Reflection.MethodBase")
+            {
+                if (split.Length < 20)
+                    return "";
+
+                string methodName = split[19];
+                string declaringType = split[14].Split(new char[] { ',' }).First();
+                return declaringType + "." + methodName;
+            }
+            else
+                return type;
+        }
+
+        /*************************************/
+    }
+}
