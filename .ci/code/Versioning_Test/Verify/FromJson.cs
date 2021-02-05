64c1e843cc5d7c1ccedd9a6d327ff5ae3644a0a4
adecler <arnaud.declercq@burohappold.com>
2021-02-05 16:04:46 +0800

    Align with changes to Test_oM and Test_Engine

--- .ci/code/Versioning_Test/Verify/FromJson.cs
+++ .ci/code/Versioning_Test/Verify/FromJson.cs
@@ -26,12 +26,14 @@ using BH.Engine.Serialiser;
 using BH.Engine.Test;
 using BH.oM.Base;
 using BH.oM.Reflection.Debugging;
+using BH.oM.Test;
 using BH.oM.Test.Results;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace BH.Test.Versioning
@@ -44,97 +46,92 @@ namespace BH.Test.Versioning
 
         public static TestResult FromJsonDatasets()
         {
-            string currentDirectory = Directory.GetCurrentDirectory();
-            string testFolder = Path.Combine(currentDirectory, @"..\Datasets\TestSets\Versioning");
+            string testFolder = @"C:\ProgramData\BHoM\Datasets\TestSets\Versioning";
+            List<string> versions = new List<string> { "3.3" };
+            string exceptions = "Grasshopper|Rhinoceros";
 
             // Test all the BHoM versions available
-            List<TestResult> results = Directory.GetDirectories(testFolder).Select(x => FromJsonDataset(x)).ToList();
+            List<TestResult> results = versions.Select(v => Path.Combine(testFolder, v)).Select(x => FromJsonDataset(x, exceptions)).ToList();
+
+            // Generate the result message
+            int errorCount = results.Where(x => x.Status == TestStatus.Error).Count();
+            int warningCount = results.Where(x => x.Status == TestStatus.Warning).Count();
 
             // Returns a summary result 
-            List<TestResult> fails = results.Where(x => x.Status == ResultStatus.Fail).ToList();
-            foreach (TestResult fail in fails)
+            return new TestResult()
             {
-                foreach (Event e in fail.Events)
-                    e.Message = fail.Description + ": " + e.Message;
-            }
-            string description = $"{results.Count} versions of the BHoM tested";
-            if (fails.Count == 0)
-                return Engine.Test.Create.PassResult(description);
-            else
-                return new TestResult(ResultStatus.Fail, fails.SelectMany(x => x.Events).ToList(), description);
+                ID = "VersioningFromJsonDatasets",
+                Description = $"Versioning from json datasets generated with {results.Count} previous beta versions of the BHoM.",
+                Message = $"{errorCount} errors and {warningCount} warnings reported.",
+                Status = results.MostSevereStatus(),
+                Information = results.Where(x => x.Status != TestStatus.Pass).ToList<ITestInformation>(),
+                UTCTime = DateTime.UtcNow,
+            };
         }
 
         /*************************************/
 
-        public static TestResult FromJsonDataset(string testFolder)
+        public static TestResult FromJsonDataset(string testFolder, string exceptions = "")
         {
             Engine.Reflection.Compute.LoadAllAssemblies();
-
-            // Read all exceptions
-            string exceptionFile = Path.Combine(testFolder, "Exceptions.json");
-            HashSet<string> objectsToIgnore = new HashSet<string>();
-            HashSet<string> methodsToIgnore = new HashSet<string>();
-            if (File.Exists(exceptionFile))
-            {
-                try
-                {
-                    string json = File.ReadAllText(exceptionFile);
-                    CustomObject custom = Engine.Serialiser.Convert.FromJson(json) as CustomObject;
-                    if (custom != null)
-                    {
-                        if (custom.CustomData.ContainsKey("Objects"))
-                        {
-                            List<object> list = custom.CustomData["Objects"] as List<object>;
-                            if (list != null)
-                                objectsToIgnore = new HashSet<string>(list.OfType<string>());
-                        }
-                        if (custom.CustomData.ContainsKey("Methods"))
-                        {
-                            List<object> list = custom.CustomData["Methods"] as List<object>;
-                            if (list != null)
-                                methodsToIgnore = new HashSet<string>(list.OfType<string>());
-                        }
-                    }
-                }
-                catch { }
-            }
+            List<TestResult> results = new List<TestResult>();
 
             // Test all objects
-            List<TestResult> fails = new List<TestResult>();
+            int nbObjects = 0;
             string objectFile = Path.Combine(testFolder, "Objects.json");
             if (File.Exists(objectFile))
             {
-                fails.AddRange(File.ReadAllLines(objectFile)
-                    .Where(x => !string.IsNullOrWhiteSpace(x))
-                    .Select(x => FromJsonItem(x, false))
-                    .Where(x => x.Status == ResultStatus.Fail && !objectsToIgnore.Contains(x.Description)));
+                IEnumerable<string> json = File.ReadAllLines(objectFile).Where(x => !string.IsNullOrWhiteSpace(x) && (exceptions.Length == 0 || !Regex.IsMatch(x, exceptions)));
+                results.AddRange(json.Select(x => FromJsonItem(x, false)));
+
+                nbObjects = json.Count();
             }
 
             // Test all methods
+            int nbMethods = 0;
             string methodFile = Path.Combine(testFolder, "Methods.json");
             if (File.Exists(methodFile))
             {
-                fails.AddRange(File.ReadAllLines(methodFile)
-                    .Where(x => !string.IsNullOrWhiteSpace(x))
-                    .Select(x => FromJsonItem(x, true))
-                    .Where(x => x.Status == ResultStatus.Fail && !methodsToIgnore.Contains(x.Description)));
+                IEnumerable<string> json = File.ReadAllLines(methodFile).Where(x => !string.IsNullOrWhiteSpace(x) && (exceptions.Length == 0 || !Regex.IsMatch(x, exceptions)));
+                results.AddRange(json.Select(x => FromJsonItem(x, true)));
+
+                nbMethods = json.Count();
             }
 
             // Returns a summary result 
-            string description = $"Version {Path.GetFileName(testFolder)}";
-            if (fails.Count == 0)
-                return Engine.Test.Create.PassResult(description);
-            else
-                return new TestResult(ResultStatus.Fail, fails.SelectMany(x => x.Events).ToList(), description);
+            string version = Path.GetFileName(testFolder);
+            int errorCount = results.Where(x => x.Status == TestStatus.Error).Count();
+            int warningCount = results.Where(x => x.Status == TestStatus.Warning).Count();
+
+            return new TestResult()
+            {
+                ID = $"VersioningFromJsonDatasets_{version}",
+                Description = $"Beta Version {Path.GetFileName(testFolder)}: {nbObjects} object types and {nbMethods} methods.",
+                Message = $"{errorCount} errors and {warningCount} warnings reported.",
+                Status = results.MostSevereStatus(),
+                Information = results.Where(x => x.Status != TestStatus.Pass).ToList<ITestInformation>(),
+                UTCTime = DateTime.UtcNow,
+            };
         }
 
         /*************************************/
 
         public static TestResult FromJsonItem(string json, bool isMethod)
         {
-            
-            object result = Engine.Serialiser.Convert.FromJson(json);
-            bool success = result != null;
+            object result = null;
+            bool detected = false;
+            try
+            {
+                Engine.Reflection.Compute.ClearCurrentEvents();
+                result = Engine.Serialiser.Convert.FromJson(json);
+                detected = BH.Engine.Reflection.Query.CurrentEvents().Any(x => x.Message.StartsWith("No upgrade for"));
+            }
+            catch (Exception e)
+            {
+                Engine.Reflection.Compute.RecordError("Deserialisation from json failed. Error: " + e.Message);
+            }
+
+            bool success = result != null || detected;
             if (!success && isMethod)
                 success = Helpers.CanReplaceMethodWithType(json);
 
@@ -144,12 +141,22 @@ namespace BH.Test.Versioning
             else
                 description = Helpers.DescriptionFromJson(json);
 
+            string message = "";
             if (!success)
-                return Engine.Test.Create.FailResult($"Failed to recover {description}", description, false);
-            else if (result is CustomObject)
-                return Engine.Test.Create.FailResult($"Object returned as CustomObject: {description}", description, false);
-            else
+                message = $"Error: Returned null from json.";
+            else if (!detected && result is CustomObject)
+                message = $"Error: Result returned as CustomObject";
+
+            if (message == "")
                 return Engine.Test.Create.PassResult(description);
+            else
+                return new TestResult
+                {
+                    Description = description,
+                    Status = TestStatus.Error,
+                    Message = message,
+                    Information = Engine.Reflection.Query.CurrentEvents().Select(x => x.ToEventMessage()).ToList<ITestInformation>()
+                };
         }
 
         /*************************************/
