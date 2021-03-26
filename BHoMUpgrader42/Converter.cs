79f7f766d071ea7fe9d3bf35759141aa136b5cb5
adecler <arnaud.declercq@burohappold.com>
2021-03-26 10:37:33 +0800

    Migrate to version 4.2

--- BHoMUpgrader42/Converter.cs
+++ BHoMUpgrader42/Converter.cs
@@ -20,40 +20,36 @@
  * along with this code. If not, see <https://www.gnu.org/licenses/lgpl-3.0.html>.      
  */
 
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Versioning_oM")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Versioning_oM")]
-[assembly: AssemblyCopyright("Copyright © https://github.com/BHoM")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+namespace BH.Upgrader.v42
+{
+    public class Converter : Base.Converter
+    {
+        /***************************************************/
+        /**** Constructors                              ****/
+        /***************************************************/
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+        public Converter() : base()
+        {
+            PreviousVersion = "4.1";
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("660d9a85-66b8-456b-b419-d3b176bbc66d")]
+        }
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.2.0.0")]
+
+        /***************************************************/
+        /**** Private Methods                           ****/
+        /***************************************************/
+
+
+
+
+        /***************************************************/
+    }
+}
 
