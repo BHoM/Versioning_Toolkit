fc1e725f2a60cac8a0b5b050759b4d22aa386c0c
Michael Hoehn <michael.hoehn@burohappold.com>
2021-06-14 11:14:39 -0600

    Add EPD to converter and add upgrades for removed objects

--- BHoMUpgrader42/Converter.cs
+++ BHoMUpgrader42/Converter.cs
@@ -41,6 +41,7 @@ namespace BH.Upgrader.v42
 
             ToNewObject.Add("BH.oM.Adapters.RAM.UniformLoadSet", UpgradeUniformLoadSet);
             ToNewObject.Add("BH.oM.CFD.CFX.AutoTimescale", UpgradeAutoTimescale);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.MaterialFragment.EnvironmentalProductDeclaration", UpgradeEnvironmentalProductDeclaration);
         }
 
 
@@ -116,6 +117,139 @@ namespace BH.Upgrader.v42
         }
 
         /***************************************************/
+
+        private static Dictionary<string, object> UpgradeEnvironmentalProductDeclaration(Dictionary<string, object> oldVersion)
+        {
+            if (oldVersion == null)
+                return null;
+
+            Dictionary<string, object> newVersion = new Dictionary<string, object>(oldVersion);
+
+            if (newVersion.ContainsKey("AcidificationPotential"))
+                newVersion.Remove("AcidificationPotential");
+
+            if (newVersion.ContainsKey("BiogenicCarbon"))
+                newVersion.Remove("BiogenicCarbon");
+
+            if (newVersion.ContainsKey("Density"))
+                newVersion.Remove("Density");
+
+            if (newVersion.ContainsKey("DepletionOfAbioticResources"))
+                newVersion.Remove("DepletionOfAbioticResources");
+
+            if (newVersion.ContainsKey("DepletionOfAbioticResourcesFossilFuels"))
+                newVersion.Remove("DepletionOfAbioticResourcesFossilFuels");
+
+            if (newVersion.ContainsKey("Description"))
+                newVersion.Remove("Description");
+
+            if (newVersion.ContainsKey("EndOfLifeTreatment"))
+                newVersion.Remove("EndOfLifeTreatment");
+
+            if (newVersion.ContainsKey("EutrophicationPotential"))
+                newVersion.Remove("EutrophicationPotential");
+
+            if (newVersion.ContainsKey("ExportedElectricalEnergy"))
+                newVersion.Remove("ExportedElectricalEnergy");
+
+            if (newVersion.ContainsKey("ExportedThermalEnergy"))
+                newVersion.Remove("ExportedThermalEnergy");
+
+            if (newVersion.ContainsKey("FreshWater"))
+                newVersion.Remove("FreshWater");
+
+            if (newVersion.ContainsKey("GlobalWarmingPotential"))
+                newVersion.Remove("GlobalWarmingPotential");
+
+            if (newVersion.ContainsKey("HazardousWasteDisposed"))
+                newVersion.Remove("HazardousWasteDisposed");
+
+            if (newVersion.ContainsKey("IndustryStandards"))
+                newVersion.Remove("IndustryStandards");
+
+            if (newVersion.ContainsKey("LifeCycleAssessmentPhase"))
+                newVersion.Remove("LifeCycleAssessmentPhase");
+
+            if (newVersion.ContainsKey("Lifespan"))
+                newVersion.Remove("Lifespan");
+
+            if (newVersion.ContainsKey("LifeCycleAssessmentPhase"))
+                newVersion.Remove("LifeCycleAssessmentPhase");
+
+            if (newVersion.ContainsKey("Manufacturer"))
+                newVersion.Remove("Manufacturer");
+
+            if (newVersion.ContainsKey("MasterFormat"))
+                newVersion.Remove("MasterFormat");
+
+            if (newVersion.ContainsKey("MaterialForEnergyRecovery"))
+                newVersion.Remove("MaterialForEnergyRecovery");
+
+            if (newVersion.ContainsKey("NonHazardousWasteDisposed"))
+                newVersion.Remove("NonHazardousWasteDisposed");
+
+            if (newVersion.ContainsKey("NonRenewableSecondaryFuels"))
+                newVersion.Remove("NonRenewableSecondaryFuels");
+
+            if (newVersion.ContainsKey("OzoneDepletionPotential"))
+                newVersion.Remove("OzoneDepletionPotential");
+
+            if (newVersion.ContainsKey("PhotochemicalOzoneCreationPotential"))
+                newVersion.Remove("PhotochemicalOzoneCreationPotential");
+
+            if (newVersion.ContainsKey("Plant"))
+                newVersion.Remove("Plant");
+
+            if (newVersion.ContainsKey("PostalCode"))
+                newVersion.Remove("PostalCode");
+
+            if (newVersion.ContainsKey("PostConsumerRecycledContent"))
+                newVersion.Remove("PostConsumerRecycledContent");
+
+            if (newVersion.ContainsKey("PrimaryEnergyNonRenewableEnergy"))
+                newVersion.Remove("PrimaryEnergyNonRenewableEnergy");
+
+            if (newVersion.ContainsKey("PrimaryEnergyNonRenewableResource"))
+                newVersion.Remove("PrimaryEnergyNonRenewableResource");
+
+            if (newVersion.ContainsKey("PrimaryEnergyRenewableEnergy"))
+                newVersion.Remove("PrimaryEnergyRenewableEnergy");
+
+            if (newVersion.ContainsKey("PrimaryEnergyRenewableTotal"))
+                newVersion.Remove("PrimaryEnergyRenewableTotal");
+
+            if (newVersion.ContainsKey("PrimaryEnergyResourcesRawMaterials"))
+                newVersion.Remove("PrimaryEnergyResourcesRawMaterials");
+
+            if (newVersion.ContainsKey("RadioActiveWasteDisposed"))
+                newVersion.Remove("RadioActiveWasteDisposed");
+
+            if (newVersion.ContainsKey("ReferenceYear"))
+                newVersion.Remove("ReferenceYear");
+
+            if (newVersion.ContainsKey("RenewableSecondaryFuels"))
+                newVersion.Remove("RenewableSecondaryFuels");
+
+            if (newVersion.ContainsKey("SecondaryMaterial"))
+                newVersion.Remove("SecondaryMaterial");
+
+            if (newVersion.ContainsKey("Type"))
+                newVersion.Add("_t", "BH.oM.LifeCycleAssessment.EPDType");
+
+            if (newVersion.ContainsKey("EnvironmentalMetric"))
+                newVersion.Add("_t", "System.Collections.Generic.List<BH.oM.LifeCycleAssessment.EnvironmentalMetric>");
+
+            if (newVersion.ContainsKey("QuantityType"))
+                newVersion.Add("_t", "BH.oM.LifeCycleAssessment.QuantityType");
+
+            if (newVersion.ContainsKey("QuantityTypeValue"))
+                newVersion.Add("_t", "System.Double");
+
+            return newVersion;
+        }
+
+        /***************************************************/
+
     }
 }
 
