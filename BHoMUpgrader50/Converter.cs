236a9856dae9617df6f421deb338898677290f29
Pawel Baran <pawel.baran@burohappold.com>
2021-11-03 23:30:59 +0100

    versioning of PinnedButtonInfo added

--- BHoMUpgrader50/Converter.cs
+++ BHoMUpgrader50/Converter.cs
@@ -38,6 +38,8 @@ namespace BH.Upgrader.v50
         public Converter() : base()
         {
             PreviousVersion = "4.3";
+
+            ToNewObject.Add("BH.oM.Revit.PinnedButtonInfo", UpgradePinnedButtonInfo);
         }
 
 
@@ -45,8 +47,54 @@ namespace BH.Upgrader.v50
         /**** Private Methods                           ****/
         /***************************************************/
 
+        private static Dictionary<string, object> UpgradePinnedButtonInfo(Dictionary<string, object> oldVersion)
+        {
+            Dictionary<string, object> newVersion = new Dictionary<string, object>(oldVersion);
+            newVersion["_t"] = "BH.oM.Revit.PinnedItemInfo";
+
+            newVersion["Method"] = "[]";
+            if (newVersion.ContainsKey("InfoString"))
+            {
+                string infoString = newVersion["InfoString"] as string;
+                string typeName, methodName;
+                List<string> parameters;
+                if (ParseInfoString(infoString, out typeName, out methodName, out parameters))
+                {
+                    Dictionary<string, object> methodProps = new Dictionary<string, object>();
+                    methodProps["_t"] = "System.Reflection.MethodBase";
+                    methodProps["TypeName"] = $"{{ \"_t\" : \"System.Type\", \"Name\" : \"{typeName}\", \"_bhomVersion\" : \"5.0\" }}";
+                    methodProps["MethodName"] = methodName;
+                    methodProps["Parameters"] = parameters.Select(x => $"{{ \"_t\" : \"System.Type\", \"Name\" : \"{x}\", \"_bhomVersion\" : \"5.0\" }}").ToList();
+                    methodProps["_bhomVersion"] = "5.0";
+                    newVersion["Method"] = methodProps;
+                }                    
+                
+                newVersion.Remove("InfoString");
+            }
+
+            return newVersion;
+        }
+
         /***************************************************/
 
+        private static bool ParseInfoString(string infostring, out string typeName, out string methodName, out List<string> parameters)
+        {
+            typeName = "";
+            methodName = "";
+            parameters = null;
+            if (string.IsNullOrWhiteSpace(infostring))
+                return false;
 
+            int parenthesis = infostring.IndexOf('(');
+            string method = infostring.Substring(0, parenthesis);
+            int dot = method.LastIndexOf('.');
+            typeName = method.Substring(0, dot);
+            methodName = method.Substring(dot + 1);
+            parameters = infostring.Substring(parenthesis + 1, infostring.Length - parenthesis - 2).Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries).Select(x => x.Replace(" ", "")).ToList();
+
+            return true;
+        }
+
+        /***************************************************/
     }
 }
