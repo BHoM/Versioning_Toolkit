d38529e368b5f2e7afb140ad00cc8e4ed15c5ccc
FraserGreenroyd <fraser.greenroyd@burohappold.com>
2022-07-18 17:38:06 +0100

    Update Assembly Information

--- BHoMUpgrader52/Properties/AssemblyInfo.cs
+++ BHoMUpgrader52/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * This file is part of the Buildings and Habitats object Model (BHoM)
  * Copyright (c) 2015 - 2022, the respective contributors. All rights reserved.
  *
@@ -28,7 +28,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BHoMUpgrader52")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("https://github.com/BHoM/Versioning_Toolkit")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BHoMUpgrader52")]
@@ -55,4 +55,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("5.0.0.0")]
-[assembly: AssemblyFileVersion("5.2.0.0")]
+[assembly: AssemblyFileVersion("5.3.0.0")]
