aa6b1b869cf5f32a3fb19178546d8e052175edf9
Isak Näslund <isak.naslund@burohappold.com>
2025-05-19 09:36:19 +0100

    Add versioning for metrics and results

--- BHoMUpgrader82/Converter.cs
+++ BHoMUpgrader82/Converter.cs
@@ -41,12 +41,166 @@ namespace BH.Upgrader.v82
         public Converter() : base()
         {
             PreviousVersion = "8.1";
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.MaterialFragments.AbioticDepletionFossilResourcesMetric", UpgradeMetric);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.MaterialFragments.AbioticDepletionMineralsAndMetalsMetric", UpgradeMetric);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.MaterialFragments.AcidificationMetric", UpgradeMetric);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.MaterialFragments.ClimateChangeBiogenicMetric", UpgradeMetric);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.MaterialFragments.ClimateChangeFossilMetric", UpgradeMetric);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.MaterialFragments.ClimateChangeLandUseMetric", UpgradeMetric);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.MaterialFragments.ClimateChangeTotalMetric", UpgradeMetric);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.MaterialFragments.ClimateChangeTotalNoBiogenicMetric", UpgradeMetric);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.MaterialFragments.EutrophicationAquaticFreshwaterMetric", UpgradeMetric);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.MaterialFragments.EutrophicationAquaticMarineMetric", UpgradeMetric);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.MaterialFragments.EutrophicationCMLMetric", UpgradeMetric);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.MaterialFragments.EutrophicationTerrestrialMetric", UpgradeMetric);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.MaterialFragments.EutrophicationTRACIMetric", UpgradeMetric);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.MaterialFragments.OzoneDepletionMetric", UpgradeMetric);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.MaterialFragments.PhotochemicalOzoneCreationCMLMetric", UpgradeMetric);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.MaterialFragments.PhotochemicalOzoneCreationMetric", UpgradeMetric);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.MaterialFragments.PhotochemicalOzoneCreationTRACIMetric", UpgradeMetric);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.MaterialFragments.WaterDeprivationMetric", UpgradeMetric);
+
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.Results.AbioticDepletionFossilResourcesMaterialResult", UpgradeMaterialResult);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.Results.AbioticDepletionMineralsAndMetalsMaterialResult", UpgradeMaterialResult);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.Results.AcidificationMaterialResult", UpgradeMaterialResult);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.Results.ClimateChangeBiogenicMaterialResult", UpgradeMaterialResult);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.Results.ClimateChangeFossilMaterialResult", UpgradeMaterialResult);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.Results.ClimateChangeLandUseMaterialResult", UpgradeMaterialResult);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.Results.ClimateChangeTotalMaterialResult", UpgradeMaterialResult);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.Results.ClimateChangeTotalNoBiogenicMaterialResult", UpgradeMaterialResult);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.Results.EutrophicationAquaticFreshwaterMaterialResult", UpgradeMaterialResult);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.Results.EutrophicationAquaticMarineMaterialResult", UpgradeMaterialResult);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.Results.EutrophicationCMLMaterialResult", UpgradeMaterialResult);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.Results.EutrophicationTerrestrialMaterialResult", UpgradeMaterialResult);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.Results.EutrophicationTRACIMaterialResult", UpgradeMaterialResult);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.Results.OzoneDepletionMaterialResult", UpgradeMaterialResult);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.Results.PhotochemicalOzoneCreationCMLMaterialResult", UpgradeMaterialResult);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.Results.PhotochemicalOzoneCreationMaterialResult", UpgradeMaterialResult);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.Results.PhotochemicalOzoneCreationTRACIMaterialResult", UpgradeMaterialResult);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.Results.WaterDeprivationMaterialResult", UpgradeMaterialResult);
+
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.Results.AbioticDepletionFossilResourcesElementResult", UpgradeElementResult);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.Results.AbioticDepletionMineralsAndMetalsElementResult", UpgradeElementResult);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.Results.AcidificationElementResult", UpgradeElementResult);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.Results.ClimateChangeBiogenicElementResult", UpgradeElementResult);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.Results.ClimateChangeFossilElementResult", UpgradeElementResult);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.Results.ClimateChangeLandUseElementResult", UpgradeElementResult);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.Results.ClimateChangeTotalElementResult", UpgradeElementResult);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.Results.ClimateChangeTotalNoBiogenicElementResult", UpgradeElementResult);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.Results.EutrophicationAquaticFreshwaterElementResult", UpgradeElementResult);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.Results.EutrophicationAquaticMarineElementResult", UpgradeElementResult);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.Results.EutrophicationCMLElementResult", UpgradeElementResult);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.Results.EutrophicationTerrestrialElementResult", UpgradeElementResult);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.Results.EutrophicationTRACIElementResult", UpgradeElementResult);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.Results.OzoneDepletionElementResult", UpgradeElementResult);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.Results.PhotochemicalOzoneCreationCMLElementResult", UpgradeElementResult);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.Results.PhotochemicalOzoneCreationElementResult", UpgradeElementResult);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.Results.PhotochemicalOzoneCreationTRACIElementResult", UpgradeElementResult);
+            ToNewObject.Add("BH.oM.LifeCycleAssessment.Results.WaterDeprivationElementResult", UpgradeElementResult);
+
         }
 
         /***************************************************/
         /**** Private Methods                           ****/
         /***************************************************/
 
+        private static Dictionary<string, object> UpgradeMetric(Dictionary<string, object> oldVersion)
+        {
+            Dictionary<string, object> newVersion = new Dictionary<string, object>();
+
+            if(oldVersion.TryGetValue("_t", out object type))
+                newVersion["_t"] = type;
+
+            newVersion["Indicators"] = IndicatorsDics(oldVersion);
+            return newVersion;
+        }
+
+        /***************************************************/
+
+        private static Dictionary<string, object> UpgradeMaterialResult(Dictionary<string, object> oldVersion)
+        {
+            Dictionary<string, object> newVersion = new Dictionary<string, object>();
+
+            if (oldVersion.TryGetValue("_t", out object type))
+                newVersion["_t"] = type;
+
+            if (oldVersion.TryGetValue("MaterialName", out object matName))
+                newVersion["MaterialName"] = matName;
+
+            if (oldVersion.TryGetValue("EnvironmentalProductDeclarationName", out object epdName))
+                newVersion["EnvironmentalProductDeclarationName"] = epdName;
+
+            newVersion["Indicators"] = IndicatorsDics(oldVersion);
+            return newVersion;
+        }
+
+        /***************************************************/
+
+        private static Dictionary<string, object> UpgradeElementResult(Dictionary<string, object> oldVersion)
+        {
+            Dictionary<string, object> newVersion = new Dictionary<string, object>();
+
+            if (oldVersion.TryGetValue("_t", out object type))
+                newVersion["_t"] = type;
+
+            if (oldVersion.TryGetValue("ObjectId", out object objId))
+                newVersion["ObjectId"] = objId;
+
+            if (oldVersion.TryGetValue("Scope", out object scope))
+                newVersion["Scope"] = scope;
+
+            if (oldVersion.TryGetValue("Category", out object cat))
+                newVersion["Category"] = cat;
+
+            if (oldVersion.TryGetValue("MaterialResults", out object matResObj))
+            {
+                newVersion["MaterialResults"] = matResObj;  //Should already be upgraded by main loop upgrading subobjects
+            }
+
+
+            newVersion["Indicators"] = IndicatorsDics(oldVersion);
+            return newVersion;
+        }
+
+        /***************************************************/
+
+        private static Dictionary<string, double> IndicatorsDics(Dictionary<string, object> oldVersion)
+        {
+            Dictionary<string, double> indicators = new Dictionary<string, double>();
+
+            List<string> propertiesToCheck = new List<string>() {
+                "A1",
+                "A2",
+                "A3",
+                "A1toA3",
+                "A4",
+                "A5",
+                "B1",
+                "B2",
+                "B3",
+                "B4",
+                "B5",
+                "B6",
+                "B7",
+                "B1to7",
+                "C1",
+                "C2",
+                "C3",
+                "C4",
+                "C1to4",
+                "D"
+            };
+
+            foreach (string prop in propertiesToCheck)
+            {
+                if (oldVersion.TryGetValue(prop, out object objVal) && objVal is double val && !double.IsNaN(val))
+                {
+                    indicators[prop] = val;
+                }
+            }
+
+            return indicators;
+        }
 
         /***************************************************/
     }
