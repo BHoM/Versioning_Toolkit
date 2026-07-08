9775665b254123084554bcc85e8eb692504f5011
Arnaud Declercq <arnaud.declercq@burohappold.com>
2026-07-08 11:21:53 +0100

    Adding local test runner exe

---
+++ .ci/code/TestRunner/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BH.oM.Test.Results;
+
+TestResult result = BH.Test.Versioning.Verify.FromJsonDatasets();
+
+Console.WriteLine("Result: " + result.Status);
+Console.WriteLine(result.Message);
